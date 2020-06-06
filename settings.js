32e10febfb04250df7ab99f75a3be5228b15c8e2
Leonardy <leonardy85@yahoo.com>
2020-06-06 14:48:37 +0800

    Set logging

--- settings.js
+++ settings.js
@@ -1,18 +1,27 @@
-module.exports =  {
+module.exports = {
   uiPort: process.env.PORT || 5000,
   userDir: ".nodered/",
   flowFile: "flows.json",
   credentialSecret: process.env.SECRET || false,
-  adminAuth: process.env.AUTH_METHOD ? require('./settings_' + process.env.AUTH_METHOD + '.js') : require('./settings.js'),
+  adminAuth: process.env.AUTH_METHOD
+    ? require("./settings_" + process.env.AUTH_METHOD + ".js")
+    : require("./settings.js"),
+  apiMaxLength: process.env.API_MAX_LENGTH,
   editorTheme: {
     page: {
-      css: "/app/css/node-red.css"
+      css: "/app/css/node-red.css",
     },
     header: {
-        "title": process.env.TITLE || "Docker node-red",
-        "image": null,
-        "url": "#"
-    }
+      title: process.env.TITLE || "Docker node-red",
+      image: null,
+      url: "#",
+    },
   },
-  apiMaxLength: process.env.API_MAX_LENGTH
-}
+  logging: {
+    console: {
+      level: "debug",
+      metrics: false,
+      audit: false,
+    },
+  },
+};
